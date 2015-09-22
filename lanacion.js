ab4b3628dd304a9263f90f802e1542fd2569975c
Joaquín Vicente <joakin@gmail.com>
2015-09-21 23:13:43 -0300

    v0.0.5: Some sections still use the old modal styles

--- lanacion.js
+++ lanacion.js
@@ -1,3 +1,7 @@
 if (document.querySelector('.lnmodal.login')) {
   document.querySelector('.lnmodal.login').remove()
 };
+if (document.querySelector('.modal-scrollable')) {
+  document.querySelector('.modal-scrollable').remove()
+};
+document.getElementsByTagName('body')[0].classList.remove("modal-open")
