233e4c786e806f180a4ae81c7230242749b3711e
Nico Fantino <nfantino@100px.com>
2017-01-13 21:19:08 +0100

    Fix for new Clarín modal
    Changed selector name and setInterval added to catch their late
    injection
    
    CSS added to avoid screen flickering

--- clarin.js
+++ clarin.js
@@ -1,5 +1,10 @@
-document.getElementsByTagName('html')[0].style.overflowY = ""
-var arr = document.querySelectorAll('.modalLoginPase')
-for (var i = 0; i < arr.length; i++) {
-  arr[i].remove()
-}
+var interval = setInterval(function() {
+  var arr = document.querySelectorAll('.modal-pase')
+  if (arr.length) {
+    document.getElementsByTagName('html')[0].style.overflowY = ""
+    for (var i = 0; i < arr.length; i++) {
+      arr[i].remove()
+    }
+    interval = null
+  }
+}, 100)
