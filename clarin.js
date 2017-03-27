0a0c01142d3f2990b1748c1feb4085da2150d47f
Juan Manuel Garcia Olivares <jm@pixel2html.com>
2017-03-27 12:33:25 -0300

    Fix para nuevo modal de Clarín

--- clarin.js
+++ clarin.js
@@ -1,10 +1,6 @@
-var interval = setInterval(function() {
-  var arr = document.querySelectorAll('.modal-pase')
-  if (arr.length) {
-    document.getElementsByTagName('html')[0].style.overflowY = ""
-    for (var i = 0; i < arr.length; i++) {
-      arr[i].remove()
-    }
-    interval = null
-  }
-}, 100)
+var interval = setInterval(function () {
+  if (document.querySelector('.lock')) {
+    document.querySelector('.lock').style.opacity = '1'
+    document.querySelector('.lock').classList.remove('lock')
+  };
+}, 500)
